c0e325e0c4625280ddeae5c1f74c81fd4dc789ce
Vera Dobrodeeva <dobro.vera@gmail.com>
2023-05-25 21:45:48 +0300

    update user part2

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, BaseUserCreationForm
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import User
 from django import forms
 from django.shortcuts import get_object_or_404
@@ -8,6 +9,7 @@ class SignUpForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(UserCreationForm, self).__init__(*args, **kwargs)
         self.fields['password1'].help_text = 'Ваш пароль должен содержать как минимум 3 символа.'
+
     class Meta:
         model = User
         fields = (
@@ -18,26 +20,65 @@ class SignUpForm(UserCreationForm):
         )
 
 
-class UpdateUserForm(UserCreationForm):
+class UpdateUserForm(UserChangeForm):
+    password = None
 
-    def __init__(self, request, *args, **kwargs):
-        super(UserCreationForm, self).__init__(*args, **kwargs)
-        self.fields['password1'].help_text = 'Ваш пароль должен содержать как минимум 3 символа.'
+    username = forms.CharField(
+        label=_("Username"),
+        widget=forms.TextInput(),
+        strip=False,
+        help_text="Хуй"
+    )
+    password1 = forms.CharField(
+        label=_("Password"),
+        strip=False,
+        widget=forms.PasswordInput(),
+        help_text='Ваш пароль должен содержать как минимум 3 символа.',
+    )
+    password2 = forms.CharField(
+        label=_("Password confirmation"),
+        widget=forms.PasswordInput(),
+        strip=False,
+        help_text=_("Enter the same password as before, for verification."),
+    )
 
-        if request == "GET":
-            self.fields['first_name'] = forms.CharField(initial=args[0]['first_name'])
-            self.fields['last_name'] = forms.CharField(initial=args[0]['last_name'])
-            self.fields['username'] = forms.CharField(initial=args[0]['username'])
-        else:
-            self.fields['first_name'] = forms.CharField(initial=request['first_name'])
-            self.fields['last_name'] = forms.CharField(initial=request['last_name'])
-            self.fields['username'] = forms.CharField(initial=request['username'])
+    def __init__(self, user_id, *args, **kwargs):
+        super(UserChangeForm, self).__init__(*args, **kwargs)
+        self.user_id = user_id
+        # self.fields['password'].help_text = 'Ваш пароль должен содержать как минимум 3 символа.'
+
+        # self.fields['first_name'] = forms.CharField(initial=args[0]['first_name'])
+        # self.fields['last_name'] = forms.CharField(initial=args[0]['last_name'])
+        # self.fields['username'] = forms.CharField(initial=args[0]['username'])
+
+    def save(self, commit=True):
+        user = User.objects.get(id=self.user_id)
+        user.first_name = self.cleaned_data.get('first_name')
+        user.last_name = self.cleaned_data.get('last_name')
+        user.username = self.cleaned_data.get('username')
+        user.password = self.cleaned_data.get('password1')
+        user.save()
+        return user
+        # print(user)
+        # user: User = super(UserChangeForm, self).save(commit=False)
+        # print(user)
+        # user.set_password(self.cleaned_data["password1"])
+        # user.username = self.cleaned_data['username']
+        # if commit:
+        #     user.save()
+        #     if hasattr(self, "save_m2m"):
+        #         self.save_m2m()
+        # return user
+
+    # def is_valid(self):
+    #     super().is_valid()
+    #     return True
 
     class Meta:
         model = User
         fields = (
             "first_name",
             "last_name",
-            "username",
-            "password1",
+            # "username",
+            # "password",
         )
