efc186dd17378da181746946c8710b19ebf20fa1
Vera Dobrodeeva <dobro.vera@gmail.com>
2023-06-15 16:06:22 +0300

    rollbar

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 
 
 def index(request):
@@ -12,9 +11,3 @@ def index(request):
 def logout_view(request):
     logout(request)
     return redirect(index)
-
-
-def inde(request):
-    a = None
-    a.hello()  # Creating an error with an invalid line of code
-    return HttpResponse("Hello, world. You're at the pollapp index.")
