139c4485709a38a28bcb6ab465c214f2368c1dd3
Vera Dobrodeeva <dobro.vera@gmail.com>
2023-06-20 16:13:28 +0300

    dj-database-url add to requirements.txt

--- task_manager/users/tests/test_views.py
+++ task_manager/users/tests/test_views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.test import TestCase, Client
 from django.urls import reverse
 
@@ -19,3 +20,17 @@ class TestViews(TestCase):
         response = self.client.get(self.create_url)
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'users/create.html')
+
+    def test_SignUpView_POST(self):
+        response = self.client.post(
+            '/users/create/',
+            {
+                'first_name': 'Test',
+                'last_name': 'Test',
+                'username': 'TestUser',
+                'password1': '12345678',
+                'password2': '12345678',
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(User.objects.filter(username="Test"))
