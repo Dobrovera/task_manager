85eaef96ef82bd7591cdbd591a2e000d0a5a40f2
Vera Dobrodeeva <dobro.vera@gmail.com>
2023-06-20 22:51:50 +0300

    labels_views tests

--- task_manager/labels/tests/test_views.py
+++ task_manager/labels/tests/test_views.py
@@ -38,3 +38,28 @@ class TestViews(TestCase):
         response = self.client.get(self.labels)
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'labels/labels.html')
+
+    def test_LabelCreateView_POST(self):
+        response = self.client.post(
+            '/labels/create/',
+            {
+                'label_name': 'label_name_1',
+            }
+        )
+        self.assertEqual(response.status_code, 302)
+        user = User.objects.get(username='test_1')
+        self.client.force_login(user)
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(Label.objects.filter(label_name="label_name_1"))
+
+    def test_LabelDeleteView_GET_and_POST(self):
+        label = Label.objects.get(label_name='test_label')
+        response = self.client.get(
+            reverse('delete_label', args=(label.id,)), follow=True
+        )
+        self.assertEquals(response.status_code, 200)
+        response = self.client.post(
+            reverse('delete_label', kwargs={'id': label.id})
+        )
+        self.assertNotContains(response, 'label_delete', status_code=302)
+        self.assertEqual(Label.objects.count(), 0)
