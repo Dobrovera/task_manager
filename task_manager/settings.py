4ca4de94e9c4803d78e5e563b606d4608eb6983f
Vera Dobrodeeva <dobro.vera@gmail.com>
2023-06-23 22:17:24 +0300

    hexlet-linter errors

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -150,12 +150,12 @@ USE_TZ = True
 
 
 MESSAGE_TAGS = {
-        messages.DEBUG: 'alert-secondary',
-        messages.INFO: 'alert-info',
-        messages.SUCCESS: 'alert-success',
-        messages.WARNING: 'alert-warning',
-        messages.ERROR: 'alert-danger',
- }
+    messages.DEBUG: 'alert-secondary',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
