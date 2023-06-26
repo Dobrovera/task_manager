1940e98327a8e7993102e917cbcec30ee6cbad8e
Vera Dobrodeeva <dobro.vera@gmail.com>
2023-06-26 14:20:40 +0300

    urls update, delete

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -3,7 +3,7 @@ from .views import UserListView, UserUpdateView, SignUpView, UserDeleteView
 
 urlpatterns = [
     path('', UserListView.as_view(), name='users'),
-    path('<int:id>/update', UserUpdateView.as_view(), name='update'),
+    path('<int:id>/update/', UserUpdateView.as_view(), name='update'),
     path('create/', SignUpView.as_view(), name='create'),
-    path('<int:id>/delete', UserDeleteView.as_view(), name='delete'),
+    path('<int:id>/delete/', UserDeleteView.as_view(), name='delete'),
 ]
